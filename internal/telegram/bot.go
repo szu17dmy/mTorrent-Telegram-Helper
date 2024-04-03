c7e794abe9569a299e658b80ef60cf76ea3c1d4c
domain <do@my-sh.dev>
2024-04-03 17:21:56 +0800

    :sparkles: Add telegram send torrent msg.

---
+++ internal/telegram/bot.go
@@ -0,0 +1,65 @@
+package telegram
+
+import (
+	"bytes"
+	"embed"
+	_ "embed"
+	"html/template"
+
+	"github.com/szu17dmy/mtorrent-telegram-helper/pkg/fs"
+	mt "github.com/szu17dmy/mtorrent-telegram-helper/pkg/mtorrent"
+	tg "github.com/szu17dmy/mtorrent-telegram-helper/pkg/telegram"
+
+	"gopkg.in/telebot.v3"
+)
+
+var (
+	//go:embed torrent_message_template.html
+	torrentMessageTemplate embed.FS
+	//go:embed nsfw_torrent_message_template.html
+	nsfwTorrentMessageTemplate embed.FS
+)
+
+type Torrent struct {
+	Id         string
+	Title      string
+	Abstract   string
+	Size       string
+	Expiration string
+}
+
+func SendTorrentMessage(torrent *mt.Torrent) (*telebot.Message, error) {
+	tpl, err := template.ParseFS(torrentMessageTemplate, "torrent_message_template.html")
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, parseTorrent(torrent))
+	if err != nil {
+		return nil, err
+	}
+	return tg.SendHtml(buf.String())
+}
+
+func SendNSFWTorrentMessage(torrent *mt.Torrent) (*telebot.Message, error) {
+	tpl, err := template.ParseFS(nsfwTorrentMessageTemplate, "nsfw_torrent_message_template.html")
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, parseTorrent(torrent))
+	if err != nil {
+		return nil, err
+	}
+	return tg.SendHtml(buf.String())
+}
+
+func parseTorrent(torrent *mt.Torrent) *Torrent {
+	return &Torrent{
+		Id:         torrent.Id,
+		Title:      torrent.Name,
+		Abstract:   torrent.Abstract,
+		Size:       fs.ByteSize(torrent.Size).String(),
+		Expiration: torrent.Status.PinExpirationDate,
+	}
+}
---
+++ internal/telegram/bot_test.go
@@ -0,0 +1,90 @@
+package telegram
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"testing"
+
+	mt "github.com/szu17dmy/mtorrent-telegram-helper/pkg/mtorrent"
+)
+
+func TestSendTorrentMessage(t *testing.T) {
+	type args struct {
+		torrent *mt.Torrent
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Sample",
+			args: args{
+				torrent: &mt.Torrent{
+					Id:       "769649",
+					Name:     "Post-graduate Entrance Examination",
+					Abstract: "2023考研资料合集",
+					Size:     1393812756406,
+					Status: mt.TorrentStatus{
+						PinExpirationDate: "2024-04-08 00:00:00",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SendTorrentMessage(tt.args.torrent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SendTorrentMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			bs, err := json.MarshalIndent(got, "", "  ")
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(string(bs))
+		})
+	}
+}
+
+func TestSendNSFWTorrentMessage(t *testing.T) {
+	type args struct {
+		torrent *mt.Torrent
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Sample",
+			args: args{
+				torrent: &mt.Torrent{
+					Id:       "769649",
+					Name:     "Post-graduate Entrance Examination",
+					Abstract: "2023考研资料合集",
+					Size:     1393812756406,
+					Status: mt.TorrentStatus{
+						PinExpirationDate: "2024-04-08 00:00:00",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SendNSFWTorrentMessage(tt.args.torrent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SendNSFWTorrentMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			bs, err := json.MarshalIndent(got, "", "  ")
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(string(bs))
+		})
+	}
+}
